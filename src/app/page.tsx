7f5aa1587b9583b3c074fffc4a475328c99c9497
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-11 16:30:49 +0100

    Add logo and header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main>
+      <h1>Find events around you</h1>
+      <p>Browse more than 10,000 events around you</p>
+      <form>
+        <input placeholder='Search events in any city...' spellCheck={false} />
+      </form>
+      <section>
+        <p>Popular:</p>
+        <div>
+          <Link href={'/events/seattle'}> Seattle</Link>
+          <Link href={'/events/austin'}>Austin</Link>
+        </div>
+      </section>
+    </main>
+  );
 }
