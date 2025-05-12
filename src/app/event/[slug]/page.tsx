5a88163577addc8e1736e71c1899c0db754797cc
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-12 21:11:03 +0100

    Add bg image to event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 type EventsPageProps = {
   params: {
     slug: string;
@@ -11,5 +13,19 @@ export default async function EventPage({ params }: EventsPageProps) {
   const response = await fetch(`${APP_URL}/${slug}`);
   const event = await response.json();
   console.log(event);
-  return <main>Event page</main>;
+  return (
+    <main className='h-[100vh]'>
+      <section className='relative h-[40%] overflow-hidden'>
+        <Image
+          src={event.imageUrl}
+          alt='Event background image'
+          fill
+          quality={50}
+          sizes='(max-width:1280px), 100vw, 1280px'
+          className='object-cover blur-3xl '
+        />
+      </section>
+      <div>second section</div>
+    </main>
+  );
 }
