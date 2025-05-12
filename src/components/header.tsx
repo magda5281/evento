ab527cec5d697f69583bbc1a36d9da349096c5bb
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-12 11:15:43 +0100

    Add animation to header links with framer-motion

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Logo from './logo';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { motion } from 'framer-motion';
 
 const routes = [
   {
@@ -19,19 +20,31 @@ export default function Header() {
   return (
     <header className='flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9'>
       <Logo />
-      <ul className='flex gap-x-6 text-sm sm:text-md'>
-        {routes.map((route) => (
-          <li
-            key={route.path}
-            className={clsx('hover:text-white transition', {
-              'text-white': activePathName === route.path,
-              'text-white/50': activePathName !== route.path,
-            })}
-          >
-            <Link href={route.path}>{route.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <nav className='h-full'>
+        {' '}
+        <ul className='flex gap-x-6 text-sm sm:text-md h-full'>
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className={clsx(
+                'hover:text-white transition relative flex items-center',
+                {
+                  'text-white': activePathName === route.path,
+                  'text-white/50': activePathName !== route.path,
+                }
+              )}
+            >
+              <Link href={route.path}>{route.name}</Link>
+              {activePathName === route.path && (
+                <motion.div
+                  layoutId={'header-active-link'}
+                  className='bg-accent h-1 w-full absolute bottom-0'
+                ></motion.div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
