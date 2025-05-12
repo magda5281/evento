ee2a14bc06e46ee8afeaf853880b388ca4c7cd35
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-12 20:50:44 +0100

    Fetch event data in event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+type EventsPageProps = {
+  params: {
+    slug: string;
+  };
+};
 
-export default function EventPage() {
+export default async function EventPage({ params }: EventsPageProps) {
+  const slug = params.slug;
+  const APP_URL = process.env.APP_URL;
+
+  const response = await fetch(`${APP_URL}/${slug}`);
+  const event = await response.json();
+  console.log(event);
   return <main>Event page</main>;
 }
