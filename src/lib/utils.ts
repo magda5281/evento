7908557c885ae72044fd9823e0dcc7ff52d07a2a
Magdalena Gozdalik <magdagozdalik28@gmail.com>
2025-05-14 16:17:35 +0100

    Refactor getEvents
    sqlite is not supporting mode: 'insensitive' option

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { EventoEvent, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 export function cn(...inputs: ClassValue[]) {
@@ -12,11 +12,11 @@ export function capitalize(str: string) {
 }
 
 export async function getEvents(city: string) {
-  const APP_URL = process.env.APP_URL;
-  const response = await fetch(`${APP_URL}?city=${city}`, {
-    next: { revalidate: 300 },
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city: capitalize(city),
+    },
   });
-  const events: EventoEvent[] = await response.json();
   return events;
 }
 
